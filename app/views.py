7c5502880998e9c79ad840c687229d4d56811abe
Ivy Kabere <kabereivy@gmail.com>
2022-04-30 12:11:05 +0300

    set up configuration

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message="a news app"
+    message="a news app on"
     return render_template('index.html',message=message)
 
 @app.route('/news/<int:news_id>')
