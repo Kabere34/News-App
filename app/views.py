636f9e943a14f5d5f571a35c81679fd56ec846cd
Ivy Kabere <kabereivy@gmail.com>
2022-04-30 12:36:21 +0300

    add control flows

--- app/views.py
+++ app/views.py
@@ -18,3 +18,17 @@ def news(news_id):
     View news page function that return the news details page and its data
     '''
     return render_template('news.html',id=news_id)
+
+def index():
+  '''
+  View root page fuction that returns the index page and its data
+  '''
+  title='Home-Welcome to the Best Preview Website'
+  return render_template('index.html',title = title)
+
+def news():
+  '''
+   View root page function that returns the news page and its data
+  '''
+  title = 'News- A moview Site'
+  return render_template('news.html', title = title)
